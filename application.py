dc861a30df748dce77448e72207cbb6dc4e7d3b0
TylerHolston <tholston@ucsd.edu>
2023-04-24 00:47:29 -0700

    testing db init

--- application.py
+++ application.py
@@ -1,14 +1,8 @@
 import os
-from flask import Flask
+from project import application, db
 
-application = Flask(__name__)
+with application.app_context():
+    db.create_all()
 
-@application.route("/")
-def test_env_variable():
-    return "Hello World"
-
-#with application.app_context():
-#    db.create_all()
-
-#if __name__=='__main__':
-#    application.run(debug=True, host="0.0.0.0", port=int(os.envion.get("PORT",8080)))
+if __name__=='__main__':
+    application.run(debug=True, host="0.0.0.0", port=int(os.envion.get("PORT",8080)))
